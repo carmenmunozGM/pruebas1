a9d0ab153426004b2ec4ef44c1dc217733641afb
carmenmunozGM <carmenmunoz@grupomendez.com>
2025-09-19 13:32:22 -0600

    Agregar archivos de proyecto.

---
+++ pruebas1/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace pruebas1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ pruebas1/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace pruebas1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
