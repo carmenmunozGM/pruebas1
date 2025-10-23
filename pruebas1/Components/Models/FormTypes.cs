60130e2ee51a5cdb69f3ee001ac907b25fa040c1
carmenmunozGM <carmenmunoz@grupomendez.com>
2025-10-23 15:42:39 -0600

    terminada

---
+++ pruebas1/Components/Models/FormTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pruebas1.Components.Models
+{
+    public enum FormTypes
+    {
+        Create,
+        Edit,
+        View,
+        Delete
+    }
+}
