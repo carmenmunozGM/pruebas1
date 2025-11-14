66b6ea709593bbe621e0500313cf52d2f8a2eafc
fernandocarrillo <fernandocarrillo@grupomendez.com>
2025-11-14 10:27:46 -0600

    cambios generales

--- pruebas1/MauiProgram.cs
+++ pruebas1/MauiProgram.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
 using pruebas1.Servicios;
+using System.Globalization;
 
 namespace pruebas1
 {
@@ -14,7 +15,13 @@ namespace pruebas1
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 });
-                     builder.Services.AddScoped<ReglasRecurrenciaServices>();
+            // 👇 Establece cultura mexicana
+            var cultura = new CultureInfo("es-MX");
+            CultureInfo.DefaultThreadCurrentCulture = cultura;
+            CultureInfo.DefaultThreadCurrentUICulture = cultura;
+
+
+            builder.Services.AddScoped<ReglasRecurrenciaServices>();
                      builder.Services.AddScoped<LoginService>();
                      builder.Services.AddMauiBlazorWebView();
                 builder.Services.AddScoped(sp =>
@@ -23,7 +30,7 @@ namespace pruebas1
                 {
                     BaseAddress = new Uri("https://redgm.site:9096/")
                 };
-
+              
                 /*var credenciales = Encoding.ASCII.GetBytes("");
                 http.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("basic", Convert.ToBase64String(credenciales));*/
 
