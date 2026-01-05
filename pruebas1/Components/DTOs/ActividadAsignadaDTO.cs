b6f4e6aa98176492dab5958e49fc3ab7eb8993db
0360 <0360@0360-1>
2026-01-05 16:50:23 -0600

    Vista de servicio al cliente dinamica para el usuario

---
+++ pruebas1/Components/DTOs/ActividadAsignadaDTO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pruebas1.Components.DTOs
+{
+    public class ActividadAsignadaDTO
+    {
+        public int IdCheckListAsignacion { get; set; }
+        public int IdActividadGeneral { get; set; }
+        public string NombreActividad { get; set; } = string.Empty;
+        public DateTime? Fecha { get; set; }
+        public int? Minuta { get; set; }
+        public string? Observaciones { get; set; }
+    }
+    public class ServicioActividadesDTO
+    {
+        public string NombreServicio { get; set; } = string.Empty;
+        public List<ActividadAsignadaDTO> Actividades { get; set; } = new();
+    }
+    public class ActividadesEmpresaDTO
+    {
+        public string NombreEmpresa { get; set; } = string.Empty;
+        public List<ServicioActividadesDTO> Servicios { get; set; } = new();
+    }
+
+}
---
+++ pruebas1/Components/DTOs/GrupoClientesAsignadosDTO.cs
@@ -0,0 +1,16 @@
+﻿using pruebas1.Components.DTOs.pruebas1.Components.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pruebas1.Components.DTOs
+{
+    public class GrupoClientesAsignadosDTO
+    {
+        public int IdGrupo { get; set; }
+        public string NombreGrupo { get; set; } = string.Empty;
+        public List<ClienteAsignadoDTO> Clientes { get; set; } = new();
+    }
+}
