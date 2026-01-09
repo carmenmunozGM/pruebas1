92d493697ab8648c4444c73cee294bf5eafbaa52
Karim-Negrete <karimnegrete@grupomendez.com>
2026-01-09 12:45:39 -0600

    Solucioné conflictos. Son el '?' en el id de salas.

--- pruebas1/Components/DTOs/EventoApiDTO.cs
+++ pruebas1/Components/DTOs/EventoApiDTO.cs
@@ -21,9 +21,8 @@ namespace pruebas1.Components
         public bool esRecurrente { get; set; }
         public string reglaRecurrencia { get; set; }
         public int idPrioridad { get; set; }
-        public int idSala { get; set; }
-        public string ubicacion { get; set; }
-
+        public int? idSala { get; set; }
+        public string? ubicacion { get; set; }
         public List<ParticipanteDTO> participantes { get; set; } = new();
 
         public int creadorId { get; set; }
