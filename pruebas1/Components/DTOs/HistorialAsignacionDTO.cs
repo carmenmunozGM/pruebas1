4b1bbd1ff20785dba6ac10898a7e1c4494349b04
0360 <0360@0360-1>
2026-01-28 13:20:16 -0600

    Asignarclientes ya muestra el historial

---
+++ pruebas1/Components/DTOs/HistorialAsignacionDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pruebas1.Components.DTOs
+{
+    public class HistorialAsignacionDTO
+    {
+        public string Empleado { get; set; } = "";
+        public string Entidad { get; set; } = "";
+        public string TipoEntidad { get; set; } = "";
+        public string Servicios { get; set; } = "";
+        public int TotalActividades { get; set; }
+        public DateTime FechaAsignacion { get; set; }
+    }
+
+}
