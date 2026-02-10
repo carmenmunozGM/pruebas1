3f5f35baaa3211f831d9f71d59f673107708d01c
fernandocarrillo <fernandocarrillo@grupomendez.com>
2026-02-10 10:49:44 -0600

    Minuta en servicio cliente(Falta mejor diseño)

---
+++ pruebas1/Components/Models/FilaMinutas.cs
@@ -0,0 +1,10 @@
+﻿public class FilaMinuta
+{
+    public string Cliente { get; set; }
+    public string Actividad { get; set; }
+    public string Periodo { get; set; }
+
+    public TimeOnly? Horas { get; set; }   // 👈 CAMBIO CLAVE
+
+    public bool CapAux { get; set; }
+}
