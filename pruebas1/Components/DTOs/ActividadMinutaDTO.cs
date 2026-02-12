a3a8da7bb7248af1c4c532c865fa98aefc323a9d
0360 <0360@0360-1>
2026-02-12 15:26:41 -0600

    Minuta funcional

---
+++ pruebas1/Components/DTOs/ActividadMinutaDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pruebas1.Components.DTOs
+{
+    public class ActividadMinutaDTO
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; } = "";
+    }
+
+}
