e8336fdf1f28d60ddf759ae60a26999fd7f0c913
Karim-Negrete <karimnegrete@grupomendez.com>
2026-02-16 12:54:13 -0600

    Perfiles completamente funcional.

---
+++ pruebas1/Components/DTOs/EditarPerfilDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pruebas1.Components.DTOs
+{
+    public class EditarPerfilDTO
+    {
+        public int ClavePersonal { get; set; }
+        public int IdPerfil { get; set; }
+    }
+}
