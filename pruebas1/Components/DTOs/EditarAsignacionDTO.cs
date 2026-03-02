74e78ace4c9e5299d24d2dd1fdfd1d5f14c5104e
0360 <0360@0360-1>
2026-03-02 13:11:14 -0600

    Editar ya es funcional

---
+++ pruebas1/Components/DTOs/EditarAsignacionDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pruebas1.Components.DTOs
+{
+   
+        public class EditarAsignacionDTO
+        {
+            public List<int> IdUsuarioEntidadAgenda { get; set; } = new();
+
+            public int IdEmpleado { get; set; }
+
+            public List<int> ActividadesIds { get; set; } = new();
+        }
+    
+}
