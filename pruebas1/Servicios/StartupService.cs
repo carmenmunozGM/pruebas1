bcafb2af7ec8c3bd974167e3bad37d497020b984
Karim-Negrete <karimnegrete@grupomendez.com>
2026-03-12 14:44:35 -0600

    Inicio automático y v 1.1.o, aún no se sube weyes, hay que preparar los cambios.

---
+++ pruebas1/Servicios/StartupService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace pruebas1.Servicios
+{
+    public class StartupService
+    {
+        public void RegisterStartup()
+        {
+#if WINDOWS
+
+            string startupFolder = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+
+            string shortcutPath = Path.Combine(startupFolder, "Agenda GM.lnk");
+
+            if (File.Exists(shortcutPath))
+                return;
+
+            // carpeta donde corre la app
+            string baseDir = AppContext.BaseDirectory;
+
+            // subir un nivel para encontrar Update.exe
+            string updateExe = Path.GetFullPath(Path.Combine(baseDir, @"..\Update.exe"));
+
+            string arguments = "--processStart pruebas1.exe";
+
+            Type shellType = Type.GetTypeFromProgID("WScript.Shell");
+            dynamic shell = Activator.CreateInstance(shellType);
+
+            dynamic shortcut = shell.CreateShortcut(shortcutPath);
+
+            shortcut.TargetPath = updateExe;
+            shortcut.Arguments = arguments;
+            shortcut.WorkingDirectory = Path.GetDirectoryName(updateExe);
+            shortcut.Description = "Agenda GM";
+
+            shortcut.Save();
+
+#endif
+        }
+    }
+}
